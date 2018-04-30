084dd50033cb664d41ebd26befe0fb0062b02f9f
Raden Muhammad <4066712+RadZaeem@users.noreply.github.com>
2018-05-01 00:10:24 +0900

    Initial commit (#7)

---
+++ cmd/config.sh
@@ -0,0 +1,16 @@
+cmd_config_help() {
+    cat <<_EOF
+    config
+        Run configuration scripts inside the container.
+
+_EOF
+}
+
+cmd_config() {
+    # run standard config scripts
+    ds inject debian-fixes.sh
+    ds inject setup.sh
+    ds inject install-plinth.sh
+}
+
+
---
+++ settings.sh
@@ -0,0 +1,8 @@
+APP=freedombox
+
+### Docker settings.
+IMAGE=freedombox1
+CONTAINER=freedombox1
+DOMAIN="freedombox1.example.org"
+#DOMAINS="dev.debian1.example.org tst.debian1.example.org"
+#PORTS="2201:22 53:53"

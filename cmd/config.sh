970e8debe892daac52adbb5c3e3dfde320165cae
dashohoxha <dashohoxha@gmail.com>
2018-05-10 21:01:47 +0200

    Fix cmd_config

--- cmd/config.sh
+++ cmd/config.sh
@@ -10,12 +10,12 @@ cmd_config() {
     ds inject debian-fixes.sh
     ds inject ssmtp.sh
 
+    [[ -d plinth ]] \
+        || git clone https://salsa.debian.org/freedombox-team/plinth
+
     ds inject fix-django-problem.sh
     ds setup
     ds inject install-dependencies.sh
-
-    [[ -d plinth ]] \
-        || git clone https://salsa.debian.org/freedombox-team/plinth
 
     ds restart
 }
